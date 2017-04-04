8b628909bb04b3fe5891adc5d680106c64b6ba0b
saka <asclepius1993@gmail.com>
2017-04-03 20:21:49 -0400

    change file name

---
+++ Airport_files/src/EventHandler.java
@@ -0,0 +1,6 @@
+/// Author: Xiaoyang MENG
+
+public interface EventHandler {
+    void handle(Event event);
+
+}
---
+++ Airport_files/src/SimulatorEvent.java
@@ -0,0 +1,10 @@
+// Author: Xiaoyang MENG
+
+
+public class SimulatorEvent extends Event {
+    public static final int STOP_EVENT = 0;
+
+    SimulatorEvent(double delay, EventHandler handler, int eventType) {
+        super(delay, handler, eventType);
+    }
+}
