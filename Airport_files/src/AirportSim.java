536ee59c76c3d743d635a9504e31e2145333d219
saka <asclepius1993@gmail.com>
2017-04-14 14:27:40 -0400

    new

--- Airport_files/src/AirportSim.java
+++ Airport_files/src/AirportSim.java
@@ -16,14 +16,7 @@ public class AirportSim {
 
     // List of airport with initialized runway time, on ground time and geographical coordinator of the airport
     // Source of coordinator : https://www.distancesto.com/coordinates/cn/beijing-latitude-longitude/history/991.html
-    /*
-    private static List<Airport> m_listOfAirport = new ArrayList<Airport>(Arrays.asList(
-            new Airport("SEA", 4, 4, new Coordinator(-122.3088165, 47.4502499)),
-            new Airport("LAX", 8, 8, new Coordinator(-118.40853, 33.9415889)),
-            new Airport("ATL", 10, 10, new Coordinator(-84.42770009, 33.6407282)),
-            new Airport("CDG", 9, 9, new Coordinator(2.541188, 49.013993)),
-            new Airport("PEK", 12, 12, new Coordinator(116.407395, 39.904211))));
-     */
+
     private static List<Airport> m_listOfAirport = new ArrayList<Airport>();
     // random variable
     private static Random rand = new Random();
